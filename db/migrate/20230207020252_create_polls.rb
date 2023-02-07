995bd6a109a072d3b49d0015ddabccf53ba4895e
ian.moss <ianmsieg@gmail.com>
2023-02-06 20:30:00 -0600

    added poll scaffolding

---
+++ db/migrate/20230207020252_create_polls.rb
@@ -0,0 +1,10 @@
+class CreatePolls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :polls do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
